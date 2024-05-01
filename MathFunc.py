bfa2cec582a572510df6bcda370f9803832920ed
Angad <angadsingh1729@gmail.com>
2024-05-01 23:57:47 +0530

    Made minor changes in mathfunc.py

--- MathFunc.py
+++ MathFunc.py
@@ -26,7 +26,7 @@ zeta_2 = 1.6449340668482264364724151
 zeta_3 = 1.2020569031595942853997381
 
 # Small primes for deterministic primality testing
-SMALL_PRIMES = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47]
+SMALL_PRIMES = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53]
 
 #Function Definitions:
 
